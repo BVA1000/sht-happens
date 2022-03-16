4f249634deb68ddf2d5a88d4e96c88a9d859d461
BenVonAchen <bennyvonachen@gmail.com>
2022-03-16 12:54:10 -0600

    Applied Bootstrap to App.js

--- sht-happens-frontend/src/App.js
+++ sht-happens-frontend/src/App.js
@@ -1,21 +1,37 @@
-import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">SHT HAPPENS®</Link>
+          <Navbar>
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand id="sht-happens">SHT HAPPENS®</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/product/:slug" element={<ProductPage />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/product/:slug" element={<ProductPage />} />
+            </Routes>
+          </Container>
         </main>
+        <footer className="text-center">
+          <div>
+            SHT HAPPENS® "SUCCESS HERE TODAY", Copyright 2022 All Rights
+            Reserved
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
